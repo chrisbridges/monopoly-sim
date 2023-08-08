5d07fc34122116d3c663f6d1f3fd8370370fe228
Chris Bridges <christopher_bridges@icloud.com>
2023-08-08 10:12:51 -0500

    rough, broken logic for keeping track of doubles and jail rolls

--- models/player.py
+++ models/player.py
@@ -1,5 +1,5 @@
 from board import MonopolyBoard
-
+# from ..utils import roll_the_dice()
 class Player:
     def __init__(self, name: str, token: str):
         self.name = name
@@ -9,10 +9,40 @@ class Player:
         self.token = token
         self.current_position = 0  # Starting at "Go"
 
-    def move(self, steps, board: MonopolyBoard):
-        for _ in range(steps):
-            self.current_position = board.traverse_board(1)
-        return self.current_position
+    def move(self, spaces):
+        # dice1, dice2 = utils.
+        if self.is_in_jail:
+            if dice1 == dice2:
+                print(f'{self.token} rolled doubles {dice1} and {dice2} and leaves the jail.')
+                self.is_in_jail = False
+                self.num_failed_attempts_to_leave_jail = 0
+            else:
+                self.num_failed_attempts_to_leave_jail += 1
+                if self.num_failed_attempts_to_leave_jail == 3:
+                    print(f'{self.token} failed to roll doubles for 3 turns in jail and pays the fine.')
+                    self.money -= 50  # Pay the fine
+                    self.is_in_jail = False
+                    self.num_failed_attempts_to_leave_jail = 0
+                else:
+                    print(f'{self.token} remains in jail as they did not roll doubles.')
+            self.num_consecutive_doubles = 0
+        else:
+            if dice1 == dice2:
+                self.num_consecutive_doubles += 1
+            else:
+                self.num_consecutive_doubles = 0
+            if self.num_consecutive_doubles == 3:
+                print(f'{self.token} rolled doubles three times in a row and goes to jail.')
+                self.is_in_jail = True
+                self.position = spaces.index(('Jail', 'Jail'))  # Send the player to jail
+                self.num_consecutive_doubles = 0
+            else:
+                self.position = (self.position + dice1 + dice2) % len(spaces)  # Move the player
+        return dice1, dice2
+
+    def take_turn(self, spaces):
+        dice1, dice2 = self.move(spaces)
+        print(f'{self.token} rolled {dice1} and {dice2} and landed on {spaces[self.position][0]}.')
 
     def buy_property(self, property_name):
         # In a real game, we'd also deduct the purchase price from the player's money
