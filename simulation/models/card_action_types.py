de3a662b8d6effd9a2d220396d89fb264868a999
Chris Bridges <christopher_bridges@icloud.com>
2025-04-01 17:58:40 -0500

    doubles quotes

--- simulation/models/card_action_types.py
+++ simulation/models/card_action_types.py
@@ -1,7 +1,8 @@
 class CardActionTypes(Enum):
-  MOVE = 'move'
-  COLLECT = 'collect'
-  PAY = 'pay'
-  JAIL_FREE = 'jail_free'
-  REPAIRS = 'repairs'
-  SEND_TO_JAIL = 'send_to_jail'
+  """Enum for action types of cards in Monopoly."""
+  MOVE = "move"
+  COLLECT = "collect"
+  PAY = "pay"
+  JAIL_FREE = "jail_free"
+  REPAIRS = "repairs"
+  SEND_TO_JAIL = "send_to_jail"
