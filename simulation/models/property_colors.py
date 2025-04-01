23d3d44c484d64a932544189a4dfe7d3bf520acc
Chris Bridges <christopher_bridges@icloud.com>
2025-04-01 17:50:51 -0500

    implemented propertycolors enum

---
+++ simulation/models/property_colors.py
@@ -0,0 +1,10 @@
+class PropertyColors(Enum):
+    """Enum for property colors in Monopoly."""
+    BROWN = "brown"
+    LIGHT_BLUE = "light_blue"
+    PINK = "pink"
+    ORANGE = "orange"
+    RED = "red"
+    YELLOW = "yellow"
+    GREEN = "green"
+    DARK_BLUE = "dark_blue"
