7dfbcd8092f944d7cef3f310ab9ce432803ff9b3
Chris Bridges <christopher_bridges@icloud.com>
2025-04-01 21:10:18 -0500

    rollback logging

--- simulation/game_engine/movement.py
+++ simulation/game_engine/movement.py
@@ -5,9 +5,9 @@ def roll_dice():
 	dice_total = random.randint(1, 6) + random.randint(1, 6)
 	return dice_total
 
-def move(player, board, logger): #TODO: rename
+def move(player, board): #TODO: rename
     dice_total = roll_dice()
-    logger.info(f"{player.name} rolls {dice_total}.")
+    print(f"{player.name} rolls {dice_total}.")
 
     old_position = player.position
     new_position = (old_position + dice_total) % len(board)
@@ -15,7 +15,7 @@ def move(player, board, logger): #TODO: rename
     # Passing GO
     if new_position < old_position:
         player.money += 200
-        logger.info(f"{player.name} passed GO! +$200. Money: {player.money}")
+        print(f"{player.name} passed GO! +$200. Money: {player.money}")
 
     player.position = new_position
     return dice_total
