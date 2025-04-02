c76409711c778a29d3b9545d2ca26487dbab27cc
Chris Bridges <christopher_bridges@icloud.com>
2025-04-01 22:04:02 -0500

    typed most properties as optional

--- simulation/models/cards.py
+++ simulation/models/cards.py
@@ -8,17 +8,17 @@ class Card:
     
     # Movement-related
     move_to_position: Optional[int] = None
-    move_nearest_utility: bool = False
-    move_nearest_railroad: bool = False
-    send_to_jail: bool = False
+    move_nearest_utility: Optional[bool] = False
+    move_nearest_railroad: Optional[bool] = False
+    send_to_jail: Optional[bool] = False
 
     # Money-related
-    amount: int = 0  # Positive for collect, negative for pay (or just interpret based on action_type)
-    collect_from_all_players: bool = False  # e.g. "Grand Opera Night", "It is your birthday"
+    amount: Optional[int] = 0  # Positive for collect, negative for pay (or just interpret based on action_type)
+    collect_from_all_players: Optional[bool] = False  # e.g. "Grand Opera Night", "It is your birthday"
     
     # Repairs-related
-    pay_per_house: int = 0
-    pay_per_hotel: int = 0
+    pay_per_house: Optional[int] = 0
+    pay_per_hotel: Optional[int] = 0
 
     # Jail-free
-    get_out_of_jail_free: bool = False
+    get_out_of_jail_free: Optional[bool] = False
