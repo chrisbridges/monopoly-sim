68261b59b9b3bd4a3c7754c6586027e22b6ea98f
Chris Bridges <christopher_bridges@icloud.com>
2025-04-03 13:38:52 -0500

    added net worth method

--- simulation/models/player.py
+++ simulation/models/player.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
-from typing import List
+from typing import List, Union
 
 from models import board
+from models.square import Property, Railroad, Utility
 
 @dataclass
 class Player:
@@ -11,7 +12,7 @@ class Player:
     in_jail: bool = False
     jail_turns: int = 0
     has_get_out_of_jail_free_card: bool = False # assume can only have 1 for now
-    owned_properties: List[int] = field(default_factory=list)  # store board positions of owned squares
+    owned_properties: List[Property] = field(default_factory=list)  # store board positions of owned squares
     # net_worth: int = 0
 
     def calculate_net_worth(self) -> int:
@@ -19,6 +20,14 @@ class Player:
         Calculate the player's net worth based on their money and owned properties.
         Across the game, values are equivalent to their purchase price, unless mortgaged.
         """
-        # TODO: need to include property values as well as value of houses
-        return self.money + sum(self.get_property_value(pos) for pos in self.owned_properties)
+        value_of_properties_and_houses = 0
 
+        for property in self.owned_properties:
+            if isinstance(property, Property):
+                value_of_properties_and_houses += property.get_property_value()
+            elif isinstance(property, Railroad):
+                value_of_properties_and_houses += property.price
+            elif isinstance(property, Utility):
+                value_of_properties_and_houses += property.price
+        
+        return self.money + value_of_properties_and_houses
